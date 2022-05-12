b9be721756853e446151dc765f3ad13aa85c3012
kuroda-mobby <kuroda@mobby.co.jp>
2022-05-12 17:39:26 +0900

    styled components適用

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LApp>
+      <CCard>
+        <CIner>
+          <img src="https://placehold.jp/300x200.png" class="img" />
+          <div class="description">
+            <CCardName>コンテンツ１</CCardName>
+            <CCardText>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </CCardText>
+          </div>
+        </CIner>
+      </CCard>
+      <CCard>
+        <CIner>
+          <img src="https://placehold.jp/300x200.png" class="img" />
+          <div class="description">
+            <CCardName>コンテンツ２</CCardName>
+            <CCardText>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </CCardText>
+          </div>
+        </CIner>
+      </CCard>
+      <CCard>
+        <CIner>
+          <img src="https://placehold.jp/300x200.png" class="img" />
+          <div class="description">
+            <CCardName>コンテンツ３</CCardName>
+            <CCardText>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </CCardText>
+          </div>
+        </CIner>
+      </CCard>
+      
+    </LApp>
+    
   );
 }
 
 export default App;
+
+
+const LApp = styled.div`
+  text-align: center;
+  display: flex;
+  flex-wrap: wrap;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-between;
+`;
+const CCard = styled.div`
+  border: 2px solid #333;
+  border-radius: 5px;
+  width: 48%;
+  margin: 20px 0;
+`;
+const CIner = styled.div`
+  padding: 20px;
+`;
+const CImage = styled.img`
+
+`;
+const CDescription = styled.div`
+
+`;
+const CCardName = styled.h1`
+  text-decoration: underline;
+`;
+const CCardText = styled.p`
+  font-style: italic;
+  text-align: left;
+`;
